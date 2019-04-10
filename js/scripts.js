ac67769e6a46fd70ff1f887d8eab9c28142973f3
Kyle Harrington and Stephen Trewick <kyle@>
2019-04-10 16:20:46 -0700

    add functioning hold/update score button. full functionality. please add style

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,107 @@
 
-$(document).ready(function() {
+  var player1Turn = true;
+  var turnPoints = 0;
+  var player1;
+  var player2;
 
+
+  function Player(name) {
+    this.name = name,
+    this.score = 0
+  }
+
+  //Function to roll the die
+  var currentScore = 0 ;
+  function roll() {
+    currentRoll = Math.floor((Math.random() * 6) + 1);
+    if (currentRoll != 1) {
+      currentScore += currentRoll;
+      $(".currentRound").text(currentScore);
+    }
+    else {
+      rolledOne();
+    }
+  }
+
+  function rolledOne() {
+    currentScore = 0;
+    player1Turn = !player1Turn;
+    $(".currentRound").text("You rolled a " + currentRoll + ", next player please!");
+  }
+
+  function switchPlayer(points) {
+
+    if (player1Turn) {
+      player1.score += points
+      displayPoints(player1);
+      if (player1.score >= 100){
+        winner(player1);
+      }
+    }
+    else if (!player1Turn) {
+      player2.score += points
+      displayPoints(player2);
+      if (player2.score >= 100) {
+        winner(player2);
+      }
+    }
+    currentScore = 0;
+    player1Turn = !player1Turn;
+    if(player1Turn) {
+      alert("Player 1's Turn!")
+    }
+      else {
+        alert("Player 2's Turn, sucka!")
+      }
+  }
+
+  function displayPoints(player) {
+    if(player1Turn){
+    $("#totalScore1").text(" " + player.score);
+    } else { $("#totalScore2").text(" " + player.score);
+      }
+  }
+
+
+function createPlayer1()  {
+  player1 = new Player($("input#nameOne").val());
+  if (!$("input#nameOne").val()) {
+     alert("Hey! What's your name?");
+  } else {
+    $(".player1").show();
+    $(".name1").hide();
+    $("#player1").text(" " + player1.name);
+  }
+};
+function createPlayer2()  {
+  player2 = new Player($("input#nameTwo").val());
+  if (!$("input#nameTwo").val()) {
+      alert("Hey! What's your name?");
+  } else {
+    $(".player2").show();
+    $(".name2").hide();
+    $("#player2").text(" " + player2.name);
+  }
+};
+
+$(document).ready(function() {
+// createPlayers
   $("#buttonOne").click(function(event) {
     event.preventDefault();
-    var oneName = $("input#nameOne").val();
-      if (oneName === "") {alert("Hey! What's your name?");
-
-      } else {
-          $(".player1").show();
-          $(".name1").hide();
-          $("#player1").text(" " + oneName);
-        }
+    createPlayer1();
   });
-
   $("#buttonTwo").click(function(event) {
     event.preventDefault();
-    var twoName = $("input#nameTwo").val();
-      if (twoName === "") {alert("Hey! What's your name?");
-
-      } else {
-          $(".player2").show();
-          $(".name2").hide();
-          $("#player2").text(" " + twoName);
-        }
+    createPlayer2();
   });
 
   $("#roll").click(function() {
-    var roll = Math.floor((Math.random() * 6) + 1);
-    $("#displayRoll").text("You rolled a " + roll);
-var totalScore1 = 0;
+    roll();
+    $("#displayRoll").text("You rolled a " + currentRoll);
+  });
 
-  if (roll === 1){
-    alert("end of your turn");
-
-
-  } else if (roll>1) {
-
-      totalScore1 = totalScore1 + roll;
-      $("#totalScore1").text(totalScore1);
-    }
-});
+  $("#bankRoll").click(function(){
+    switchPlayer(currentScore);
+  });
 
 });
